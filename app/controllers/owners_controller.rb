77cce45190835a585ce0e76b16bcc3f37e421192
Joshua Burger <joshberger6969@outlook.com>
2021-02-04 21:17:40 -0500

    commit tree eb1c56ea6c5ca43c3681bade35abe99dfb556255

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,15 +2,20 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
   get '/owners/new' do 
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
